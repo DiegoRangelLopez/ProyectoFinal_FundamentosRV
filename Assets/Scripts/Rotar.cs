116b69022ba32eb160c4d1c7f0f92bacddee89b8
ManuelVerutti <79605813+ManuelVerutti@users.noreply.github.com>
2023-04-15 09:26:15 -0500

    Creación Script

---
+++ Assets/Scripts/Rotar.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotar : MonoBehaviour
+{
+
+    public float rotationSpeed = 10.0f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+
+
+}
